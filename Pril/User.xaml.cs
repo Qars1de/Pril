588589452491aedd8c96480af0ea12e15e57cf04
Антон <fiery2017@yandex.ru>
2024-01-31 11:50:25 +0300

    initial commit

---
+++ Pril/User.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Pril
+{
+    /// <summary>
+    /// Логика взаимодействия для User.xaml
+    /// </summary>
+    public partial class User : Window
+    {
+        public User()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            DB db = new DB();
+            SqlCommand command = new SqlCommand("Select * From [5Client]", db.getConnection());
+            DataTable dt = new DataTable();
+
+            db.openConnection();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            adapter.Fill(dt);
+            vivod.ItemsSource = dt.DefaultView;
+            db.closeConnection();
+        }
+    }
+}
---
+++ Pril/log.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pril
+{
+    /// <summary>
+    /// Логика взаимодействия для log.xaml
+    /// </summary>
+    public partial class log : Window
+    {
+        public log()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (logBox.Text == "" || passBox.Text == "")
+            {
+                MessageBox.Show("Заполните поля");
+            }
+            else
+            {
+
+
+                DB db = new DB();
+
+                DataTable table = new DataTable();
+
+                SqlDataAdapter adapter = new SqlDataAdapter();
+
+
+                SqlCommand command = new SqlCommand("SELECT * FROM klients WHERE login = @login AND Pass= @pass", db.getConnection());
+                command.Parameters.Add("@login", SqlDbType.VarChar).Value = logBox.Text;
+                command.Parameters.Add("@pass", SqlDbType.VarChar).Value = passBox.Text;
+
+                adapter.SelectCommand = command;
+                adapter.Fill(table);
+
+                if (table.Rows.Count > 0)
+                {
+                    this.Hide();
+                    User f = new User();
+                    f.Show();
+                }
+
+                else
+                    MessageBox.Show("Неверный номер счёта или пин-код");
+            }
+        }
+    }
+}
